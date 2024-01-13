d5ba0a0e683cfc876b88402a0c916dc978ebcb38
gabrielhyimah <gabrielamanfogyimah@gmail.com>
2024-01-13 01:18:52 +0000

    Added files for task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,30 +1,7 @@
 #!/usr/bin/env python3
-"""ALX SE Backend - i18N"""
+""" module 5-app.py """
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
-from typing import Union, Mapping
-import logging
-
-
-class Config(object):
-    """Configuration for babel."""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app = Flask(__name__)
-app.config.from_object(Config)
-babel = Babel(app)
-
-
-@babel.localeselector
-def get_locale() -> str:
-    """Return the best match of locale to use"""
-    locale = request.args.get('locale')
-    if locale:
-        return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 users = {
@@ -35,27 +12,46 @@ users = {
 }
 
 
+class Config(object):
+    """ config class for Babel """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ gets best match for locales """
+    if 'locale' in request.args and request.args.get(
+            'locale') in app.config['LANGUAGES']:
+        return request.args.get('locale')
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 def get_user():
-    """Return a dict of user data"""
-    param = request.args.get('login_as')
-    if not param:
+    """ returns a user dict of user data"""
+    userId = request.args.get('login_as')
+    try:
+        return users[int(userId)]
+    except (KeyError, ValueError, TypeError):
         return None
-    user_id = int(param)
-    user = users.get(user_id)
-    return user
 
 
 @app.before_request
 def before_request():
-    """Set user"""
-    user = get_user()
-    g.user = user
+    """ find a user if any"""
+    g.user = get_user()
 
 
-@app.route('/')
-def index():
-    """Render 5-index.html"""
-    return render_template("5-index.html")
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """ Basic Flask App """
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
